78a7ea89e0daf78bdf0c1505919248246cf03a5f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-03-07 23:24:53 +0700

    unused statement

--- test/Router/Console/CatchallTest.php
+++ test/Router/Console/CatchallTest.php
@@ -3,14 +3,13 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace ZendTest\Mvc\Router\Console;
 
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\Console\Request as ConsoleRequest;
 use Zend\Mvc\Router\Console\Catchall;
 
 class CatchallTest extends TestCase
