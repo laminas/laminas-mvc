d83d57e2e64e240e94efcc43d5b1483919cb65a0
Aleksei Khudiakov <aleksey@xerkus.pro>
2018-01-05 17:28:14 +1000

    Update copyright headers

--- src/DispatchListener.php
+++ src/DispatchListener.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @see       https://github.com/zendframework/zend-mvc for the canonical source repository
- * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      http://github.com/zendframework/zend-mvc for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-mvc/blob/master/LICENSE.md New BSD License
  */
 
--- test/Controller/Plugin/TestAsset/ListenerStub.php
+++ test/Controller/Plugin/TestAsset/ListenerStub.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-mvc/blob/master/LICENSE.md New BSD License
+ */
+
 namespace ZendTest\Mvc\Controller\Plugin\TestAsset;
 
 class ListenerStub
