b6c3c6780611ae7544fdc067a229e9d66507ce47
Aleksei Khudiakov <aleksey@xerkus.pro>
2018-05-17 10:00:07 +1000

    Add full namespace identifier for controller events

---
+++ test/Controller/TestAsset/AbstractControllerStub.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-mvc for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-mvc/blob/master/LICENSE.md New BSD License
+ */
+
+namespace ZendTest\Mvc\Controller\TestAsset;
+
+use Zend\Mvc\Controller\AbstractController;
+use Zend\Mvc\MvcEvent;
+
+class AbstractControllerStub extends AbstractController
+{
+    public function onDispatch(MvcEvent $e)
+    {
+        // noop
+    }
+}
