23b16297e8b25fe3bbcb59a23d8835b72a88e13c
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:30:50 -0600

    Rewriting as Laminas Project package

--- src/I18n/DummyTranslator.php
+++ src/I18n/DummyTranslator.php
@@ -1,15 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mvc for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Mvc\I18n;
+namespace Laminas\Mvc\I18n;
 
-use Zend\I18n\Translator\TranslatorInterface as I18nTranslatorInterface;
+use Laminas\I18n\Translator\TranslatorInterface as I18nTranslatorInterface;
 
 class DummyTranslator implements I18nTranslatorInterface
 {
