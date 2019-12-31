78a4698580f02da9ab5acc012ce515bca1ff2933
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:29:25 -0600

    Rewriting as Laminas Project package

--- test/Service/RouterFactoryTest.php
+++ test/Service/RouterFactoryTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mvc for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Mvc\Service;
+namespace LaminasTest\Mvc\Service;
 
+use Laminas\Mvc\Router\RoutePluginManager;
+use Laminas\Mvc\Service\RouterFactory;
+use Laminas\ServiceManager\ServiceManager;
 use PHPUnit_Framework_TestCase as TestCase;
-use Zend\Mvc\Router\RoutePluginManager;
-use Zend\Mvc\Service\RouterFactory;
-use Zend\ServiceManager\ServiceManager;
 
 class RouterFactoryTest extends TestCase
 {
@@ -27,28 +26,28 @@ class RouterFactoryTest extends TestCase
     {
         $this->services->setService('Config', array(
             'router' => array(
-                'router_class' => 'ZendTest\Mvc\Service\TestAsset\Router',
+                'router_class' => 'LaminasTest\Mvc\Service\TestAsset\Router',
             ),
             'console' => array(
                 'router' => array(
-                    'router_class' => 'ZendTest\Mvc\Service\TestAsset\Router',
+                    'router_class' => 'LaminasTest\Mvc\Service\TestAsset\Router',
                 ),
             ),
         ));
 
         $router = $this->factory->createService($this->services, 'router', 'Router');
-        $this->assertInstanceOf('ZendTest\Mvc\Service\TestAsset\Router', $router);
+        $this->assertInstanceOf('LaminasTest\Mvc\Service\TestAsset\Router', $router);
     }
 
     public function testFactoryCanCreateRouterWhenOnlyHttpRouterConfigPresent()
     {
         $this->services->setService('Config', array(
             'router' => array(
-                'router_class' => 'ZendTest\Mvc\Service\TestAsset\Router',
+                'router_class' => 'LaminasTest\Mvc\Service\TestAsset\Router',
             ),
         ));
 
         $router = $this->factory->createService($this->services, 'router', 'Router');
-        $this->assertInstanceOf('Zend\Mvc\Router\Console\SimpleRouteStack', $router);
+        $this->assertInstanceOf('Laminas\Mvc\Router\Console\SimpleRouteStack', $router);
     }
 }
