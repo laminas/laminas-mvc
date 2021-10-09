3248cdea745f4f7547ee1713d9f18c63fb2d9725
Andi Rückauer <arueckauer@gmail.com>
2021-10-09 17:55:52 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- test/_autoload.php
+++ test/_autoload.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-mvc for the canonical source repository
- * @copyright https://github.com/laminas/laminas-mvc/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-mvc/blob/master/LICENSE.md New BSD License
- */
-
 if (! class_exists(PHPUnit_Framework_Assert::class)
     && class_exists(PHPUnit\Framework\Assert::class)
 ) {
